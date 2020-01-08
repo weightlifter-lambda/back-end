cfc3561fea96619c4923902224c53d7a36df094c
NICOLE HOLLIS <nicolehollis@NICOLEs-MacBook-Pro.local>
2020-01-08 13:28:39 -0800

    GET and POST endpoints complete

--- api/journal/journal-router.js
+++ api/journal/journal-router.js
@@ -1,19 +1,49 @@
-const journalRouter = require("express").Router();
+const express = require("express");
+const knex = require("../../database/dbConfig.js");
+const router = require("express").Router();
 
-const Journal = require("./journal-model.js");
+// const Journal = require("./journal-model.js");
 // const restricted = require("../auth/restricted-middleware.js");
 
 
-journalRouter.get("/", (req, res) => {
-  Journal.find()
+router.get("/", (req, res) => {
+    knex
+    .select("*")
+    .from("journals")
     .then(entries => {
-      res.json(entries);
+      res.status(200).json(entries);
     })
-    .catch(err => {console.log(err)
-      res.send(err)
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ errorMessage: "Error getting the entries" });
     });
 });
 
+router.post("/", (req, res) => {
 
+    const journalData = req.body;
+  
+    knex("journals")
+      .insert(journalData)
+      .then(ids => {
+  
+        const id = ids[0];
+  
+        return knex("journals")
+          .select("exercise", "weight", "sets", "reps", "notes")
+        //   .where({ id })
+        //   .first()
+          .then(entry => {
+            res.status(201).json(entry);
+          });
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({
+          errorMessage: "Error adding the entry"
+        });
+      });
+  });
+  
 
 module.exports = router;
